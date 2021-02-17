f03ca3290c19271677262a97f1aaf3d0e7033958
SpleencannonStudent <59928035+SpleencannonStudent@users.noreply.github.com>
2021-02-17 18:08:30 +0000

    UI page functionality
    Test scene with 6-pane UI, page count scales with number of items in the readFrom list in TempList class, each page displays the objects relevant to that page.

---
+++ Assets/Scripts/Alex's_UI_Scripts/TempListScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempListScript : MonoBehaviour
+{
+    public List<string> readFrom = new List<string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        readFrom.Add("Mercury");
+        readFrom.Add("Venus");
+        readFrom.Add("Earth");
+        readFrom.Add("Mars");
+        readFrom.Add("Jupiter");
+        readFrom.Add("Saturn");
+        readFrom.Add("Uranus");
+        readFrom.Add("Neptune");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Alex's_UI_Scripts/UI_Controller.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Controller : MonoBehaviour
+{
+    [SerializeField] private GameObject AssetRepository;
+    [SerializeField] private GameObject ObjectsHide;
+    [SerializeField] private GameObject PageCounter;
+    private List<Text> objectDisplay = new List<Text>();
+    [SerializeField] private GameObject Object1;
+    [SerializeField] private GameObject Object2;
+    [SerializeField] private GameObject Object3;
+    [SerializeField] private GameObject Object4;
+    [SerializeField] private GameObject Object5;
+    [SerializeField] private GameObject Object6;
+    private TempListScript Resources;
+    Text countText;
+    int listLength;
+    int pageCount;
+    int pageCurrent = 1;
+
+    public bool increment = false;
+    public bool decrement = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region set variables
+        objectDisplay.Add(Object1.GetComponent<Text>());
+        objectDisplay.Add(Object2.GetComponent<Text>());
+        objectDisplay.Add(Object3.GetComponent<Text>());
+        objectDisplay.Add(Object4.GetComponent<Text>());
+        objectDisplay.Add(Object5.GetComponent<Text>());
+        objectDisplay.Add(Object6.GetComponent<Text>());
+        countText = PageCounter.GetComponent<Text>();
+        Resources = GetComponent<TempListScript>();
+        if (!Resources)
+        {
+            Debug.Log("Something went wrong");
+        }
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            MenuSetup();
+        }
+
+    }
+
+    private void MenuSetup()
+    {
+        //Creates a total page count based on number of objects in Resources.readFrom
+        //Includes all full pages, plus a page for the remainder
+        listLength = Resources.readFrom.Count;
+        Debug.Log(listLength);
+        pageCount = listLength / 6;
+        if(listLength % 6 > 0)
+        {
+            pageCount++;
+        }
+
+        DisplayPageDetails();
+        
+        //Displays the AssetsRepository
+        //(And 6 assets panes, if they were independently set inactive for any reason)
+        AssetRepository.SetActive(true);
+        ObjectsHide.SetActive(true);
+    }
+
+    private void DisplayPageDetails()
+    {
+        //Displays values in Resources.readFrom, in objectDisplay
+        //Starts at the relevant point for each page, should scale indefinitely.
+        countText.text = pageCurrent.ToString() + " / " + pageCount.ToString();
+        for (int i = 0; i <= 5; i++)
+        {
+            if (((pageCurrent - 1) * 6) + i > (Resources.readFrom.Count - 1))
+            {
+                objectDisplay[i].text = " ";
+            }
+            else
+            {
+                objectDisplay[i].text = Resources.readFrom[((pageCurrent - 1) * 6) + i];
+            }
+        }
+    }
+
+    public void IncrementPage()
+    {
+        pageCurrent++;
+        pageCurrent = Mathf.Clamp(pageCurrent, 1, pageCount);
+        ObjectsHide.SetActive(false);
+        DisplayPageDetails();
+        ObjectsHide.SetActive(true);
+    }
+
+    public void DecrementPage()
+    {
+        pageCurrent--;
+        pageCurrent = Mathf.Clamp(pageCurrent, 1, pageCount);
+        ObjectsHide.SetActive(false);
+        DisplayPageDetails();
+        ObjectsHide.SetActive(true);
+    }
+}
