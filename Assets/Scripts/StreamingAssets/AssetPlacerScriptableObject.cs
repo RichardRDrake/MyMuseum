bf5d68ab636c069768ca84268228b1a097edb502
larsbetting-abertay <58629214+larsbetting-abertay@users.noreply.github.com>
2021-03-03 18:44:50 +0000

    Scriptable Object

---
+++ Assets/Scripts/StreamingAssets/AssetPlacerScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetPlacerScriptableObject : ScriptableObject
+{
+    public string prefabName;
+
+    public int numberOfPrefabsToCreate;
+    public Vector3[] spawnPoints;
+}
