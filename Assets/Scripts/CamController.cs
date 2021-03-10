bb8dffb11530ffbbd933895fc99fccfcc1eca9d1
larsbetting-abertay <58629214+larsbetting-abertay@users.noreply.github.com>
2021-03-10 14:19:11 +0000

    Update CamController.cs

--- Assets/Scripts/CamController.cs
+++ Assets/Scripts/CamController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class CamController : MonoBehaviour
 {
-
-    public GameObject cam1;
-    public GameObject cam2;
     
     //Top object in each UI hierarchy
     [SerializeField] private GameObject UiBuild;
@@ -19,28 +16,51 @@ public class CamController : MonoBehaviour
     private UI_MenuController UI_MenuController;
 
     bool UiToggle = false;
+    bool cameraToggle = false;
+
+    //camera movement variables
+
+    private Transform parent;
+    private float moveSpeed = 5.0f;
+    private float rotation = 90.0f;
+
+    public float horSensitivity = 1.0f;
+    public float verSensitivity = 1.0f;
+
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         // By default, the game begins using the third person cam
-        cam1.SetActive(true);
-        cam2.SetActive(false);
+       
         UI_Controller = UiBuild.GetComponent<UI_Controller>();
         UI_MenuController = UiMain.GetComponent<UI_MenuController>();
         UI_ViewController = UiView.GetComponent<UI_ViewController>();
+
+        parent = transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // If "k" is pressed,
         if (Input.GetKeyUp("k"))
         {
+            if(cameraToggle == false)
+            {
+                cameraToggle = true;
+            }
+            else
+            {
+                cameraToggle = false;
+            }
+
             // Flips active states on cameras (e.g. if cam1 is active, cam1 is now inactive and cam2 is now active, after pressing "k")
             UI_MenuController = UiMain.GetComponent<UI_MenuController>();
-            cam1.SetActive(!cam1.activeSelf);
-            cam2.SetActive(!cam2.activeSelf);
+            //  cam2.SetActive(!cam2.activeSelf);
             UI_Controller.ResetBuildUI();
             UiToggle = !UiToggle;
             if(UiToggle == false)
@@ -56,6 +76,73 @@ public class CamController : MonoBehaviour
                 UI_MenuController.buildMode = false;
             }
             UiMain.SetActive(false);
+        }
+
+        if(cameraToggle == false)
+        {
+            thirdPersonCameraUpdate();
+        }
+        else
+        {
+            firstPersonCameraUpdate();
+        }
+    }
+
+   
+    void MouseControl()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * verSensitivity;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60, 60);
+
+        parent.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+
+    }
+    void firstPersonCameraUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            // Sets y value to 0 so the player won't move based on tilt.
+            var forward = parent.forward;
+            forward.y = 0;
+
+            // Applys the translation based on the objects rotation.
+            parent.Translate(forward * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            parent.Translate(-transform.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            var forward = parent.forward;
+            forward.y = 0;
+            parent.Translate(-forward * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            parent.Translate(transform.right * moveSpeed * Time.deltaTime, Space.World);
+        }
+
+        MouseControl();
+    }
+    void thirdPersonCameraUpdate()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            // Applys a rotation of 90 degrees to the object
+            parent.Rotate(-Vector3.up * rotation, Space.World);
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            parent.Rotate(Vector3.up * rotation, Space.World);
         }
     }
 }
