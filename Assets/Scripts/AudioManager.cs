a3a1baeaf4a6f0cfa8d4b90cd3f5fd0ea8ff268e
larsbetting-abertay <58629214+larsbetting-abertay@users.noreply.github.com>
2021-03-24 12:33:25 +0000

    Update room object and camera manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum Source { Music }
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [SerializeField] AudioSource firstAudioSource; //Create music source variable
+
+    // create clip variables
+
+    [SerializeField] AudioClip backgroundMusic; 
+
+    Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>(); //add all clips to dictionary
+
+    void Awake()
+    {
+        Instance = this;
+        _audioClips.Add("happy-music", backgroundMusic); //load clips
+
+    }
+
+    public IEnumerator PlayClip(string clip, bool loop, float volume, Source source, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PlayClip(clip, loop, volume, source); //play clip
+        yield return null;
+    }
+
+    public void PlayClip(string clip, bool loop, float volume, Source source)
+    {
+        AudioSource targetSource = null;
+        if (source == Source.Music)
+        {
+            targetSource = firstAudioSource;
+        }
+
+        targetSource.volume = volume;
+        targetSource.clip = _audioClips[clip];
+        targetSource.loop = loop;
+        targetSource.Play();
+    }
+
+    // You can use playoneshot to play multiple sounds simultaneously
+    public void PlayOneShot(string clip, float volume)
+    {
+        firstAudioSource.PlayOneShot(_audioClips[clip], volume);
+    }
+
+
+}
+
