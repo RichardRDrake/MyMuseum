d8f7f0da0ea8160b2c6cdef63cc2f1029ea2dc1a
SpleencannonStudent <59928035+SpleencannonStudent@users.noreply.github.com>
2021-04-28 13:30:27 +0100

    PUSHING IMMEDIATELY

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -8,10 +8,12 @@ public class Sound
 
     public AudioClip clip;
 
-    [Range(0f,1f)]
+    [Range(0f, 1f)]
     public float volume;
-    [Range(0.1f,3f)]
+    [Range(0.1f, 3f)]
     public float pitch;
+
+    public bool loop;
 
     [HideInInspector]
     public AudioSource source;
