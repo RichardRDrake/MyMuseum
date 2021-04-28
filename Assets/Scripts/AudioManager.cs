f9fa679802d19de74e5ce69d0c72a1d4f844850e
larsbetting-abertay <1802430@uad.ac.uk>
2021-04-28 12:12:22 +0100

    AudioManager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,68 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
+using System;
 using UnityEngine;
 public enum Source { Music }
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance;
-
-    [SerializeField] AudioSource firstAudioSource; //Create music source variable
-
-    // create clip variables
-
-    [SerializeField] AudioClip backgroundMusicFirst;
-    [SerializeField] AudioClip TitleMusic;
-    [SerializeField] AudioClip Click;
-    [SerializeField] AudioClip Can_Not_Place_Here;
-    [SerializeField] AudioClip Delete;
-    [SerializeField] AudioClip Place;
-    [SerializeField] AudioClip Select_Artifact;
-    [SerializeField] AudioClip Title;
-    Dictionary<string, AudioClip> _audioClips = new Dictionary<string, AudioClip>(); //add all clips to dictionary
+    //new audiomanager
+    public Sound[] sounds;  
 
     void Awake()
     {
-        Instance = this;
-        _audioClips.Add("Background_1st_Person_Music", backgroundMusicFirst);
-        _audioClips.Add("Background_3d_Person_Music", TitleMusic);
-        _audioClips.Add("Click_fx", Click);
-        _audioClips.Add("Can_Not_Place_Here_fx", Can_Not_Place_Here);
-        _audioClips.Add("Delete_fx", Delete);
-        _audioClips.Add("Place_fx", Place);
-        _audioClips.Add("Select_Artifact_fx", Select_Artifact);
-        _audioClips.Add("Title_Track_Music", Title)
-
-        //  _audioClips.Add("background_3d_person", backgroundMusicThird);
-
-    }
-
-    public IEnumerator PlayClip(string clip, bool loop, float volume, Source source, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        PlayClip(clip, loop, volume, source); //play clip
-        yield return null;
-    }
-
-    public void PlayClip(string clip, bool loop, float volume, Source source)
-    {
-        AudioSource targetSource = null;
-        if (source == Source.Music)
+        foreach(Sound s in sounds)
         {
-            targetSource = firstAudioSource;
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
         }
 
-        targetSource.volume = volume;
-        targetSource.clip = _audioClips[clip];
-        targetSource.loop = loop;
-        targetSource.Play();
     }
 
-    // You can use playoneshot to play multiple sounds simultaneously
-    public void PlayOneShot(string clip, float volume)
+    public void Play(string name)
     {
-        firstAudioSource.PlayOneShot(_audioClips[clip], volume);
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
+
+    
 
 
 }
