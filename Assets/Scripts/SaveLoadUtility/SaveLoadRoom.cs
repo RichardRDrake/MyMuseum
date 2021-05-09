59a9f3cc48c0bb816a1f94a5c8e5ea726519b4d9
SpleencannonStudent <59928035+SpleencannonStudent@users.noreply.github.com>
2021-05-09 21:41:03 +0100

    Updated the list of Naughty Names
    So that someone doesn't accidentally lock themselves out by naming a room NO DATA

--- Assets/Scripts/SaveLoadUtility/SaveLoadRoom.cs
+++ Assets/Scripts/SaveLoadUtility/SaveLoadRoom.cs
@@ -284,7 +284,7 @@ public class SaveLoadRoom : MonoBehaviour
 
         //There are also a bunch of bad names that may brick a computer if we allow them
         string[] badNames = { "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9"
-                                , "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"};
+                                , "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9", "NO DATA"};
 
         if (Array.Exists(badNames, element => element == str))
         {
