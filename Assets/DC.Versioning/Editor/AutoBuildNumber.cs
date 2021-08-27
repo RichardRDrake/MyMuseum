debd98ae9bff91f9e200a0a8a8636fea4d8e561c
Richard Drake <rrdrake121@gmail.com>
2021-08-27 16:55:11 +0100

    Buildable
    Changed Company Name to CPK
    Changed versioning to X.X.X format
    Versioning is displayed in the Start Menu
    Last number gets incremented before build, if you want a build to be 0.2.0 for example, set it to 0.2.-1 before building.

---
+++ Assets/DC.Versioning/Editor/AutoBuildNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class AutoBuildNumber : IPreprocessBuildWithReport
+{
+
+    private static int PreviousMinor;
+
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        //A new build has happened so lets increase our version number
+
+        string CurrentVersion = PlayerSettings.bundleVersion;
+        string[] versionElements = CurrentVersion.Split('.');
+        int[] intElements = new int[3];
+        intElements[0] = int.Parse(versionElements[0]);
+        intElements[1] = int.Parse(versionElements[1]);
+        intElements[2] = int.Parse(versionElements[2]);
+        intElements[2]++;
+        //if (intElements[1] != PreviousMinor)
+        //{
+        //    intElements[2] = 0;
+        //    PreviousMinor = intElements[1];
+
+        //}
+        PlayerSettings.bundleVersion = string.Format("{0}.{1}.{2}", intElements[0], intElements[1], intElements[2]);
+    }
+}
+
---
+++ Assets/DC.Versioning/VersionDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionDisplay : MonoBehaviour {
+
+    public string _Suffix = "";
+
+    [SerializeField] Text VersionText;
+    [SerializeField] TextMeshProUGUI VersionTextTMP;
+
+    void Start ()
+    {
+        if(VersionText)
+            VersionText.text = "Version: " + Application.version + " " + _Suffix;
+        else if (VersionTextTMP)
+            VersionTextTMP.text = "Version: " + Application.version + " " + _Suffix;
+    }
+
+}
